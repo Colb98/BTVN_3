b383c911b7a21546798815a3f24e6295b2f5f596
Loc Nguyen <1612343@student.hcmus.edu.vn>
2019-10-25 20:41:31 +0700

    Add UI

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,8 +12,16 @@ public class PlayerController : MonoBehaviour
     bool jump;
     bool walk;
     int jumpCount = 0;
+    int itemCount = 0;
     public float velocity = 0.1f;
     public float jumpForce = 5000f;
+    public TextMeshProUGUI hpText;
+    public Image hpMask;
+    public Image hpBar;
+    public GameObject panel;
+    public int maxHp = 500;
+    public float hp = 400f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +31,8 @@ public class PlayerController : MonoBehaviour
         walk = jump = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateHorizontalPosition(Vector2 pos)
     {
-        Vector2 pos = transform.position;
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
             if (Input.GetKey(KeyCode.LeftArrow))
@@ -49,11 +57,14 @@ public class PlayerController : MonoBehaviour
             a.SetBool("Walk", false);
             walk = false;
         }
+    }
 
+    void UpdateJump()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
-            if(jumpCount < 2)
+
+            if (jumpCount < 2)
             {
                 if (walk)
                 {
@@ -68,9 +79,84 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void UpdateHP()
+    {
+        hpText.SetText((int)hp + "/" + maxHp);
+        hpMask.rectTransform.transform.localScale = new Vector3(hp / maxHp, 1, 1);
+        if (hp < 100)
+        {
+            hpBar.color = new Color(1, 0, 0);
+        }
+        else
+        {
+            hpBar.color = new Color(1, 1, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        UpdateHorizontalPosition(pos);
+        UpdateJump();
+        UpdateHP();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         a.SetBool("Jumping", false);
         jumpCount = 0;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "heal":
+                Heal(0.5f);
+                break;
+            case "dmg":
+                TakeDamage(2f);
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "item":
+                PickItem(collision.gameObject);
+                break;
+        }
+    }
+
+    void PickItem(GameObject gameObject)
+    {
+        Sprite sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        gameObject.SetActive(false);
+        Vector3 pos = panel.transform.position;
+        var item = new GameObject();
+        item.transform.SetParent(panel.transform, false);
+        item.AddComponent<Image>();
+        item.GetComponent<Image>().sprite = sprite;
+        pos.x += 50 + 90 * itemCount;
+        item.transform.position = pos;
+        item.transform.localScale = new Vector3(0.6f, 0.6f);
+        itemCount++;
+    }
+
+    public void Heal(float amount)
+    {
+        hp += amount;
+        if (hp > maxHp)
+            hp = maxHp;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        hp -= amount;
+        if (hp < 1)
+            hp = 1;
     }
 }
---
+++ Assets/StreamVideo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+
+        InvokeRepeating("checkOver", .1f, .1f);
+        audioSource.Play();
+    }
+
+    private void checkOver()
+    {
+        long playerCurrentFrame = videoPlayer.GetComponent<VideoPlayer>().frame;
+        long playerFrameCount = Convert.ToInt64(videoPlayer.GetComponent<VideoPlayer>().frameCount);
+        
+        if (playerCurrentFrame < playerFrameCount - 2)
+        {
+            // print("VIDEO IS PLAYING");
+        }
+        else
+        {
+            //print("VIDEO IS OVER");
+            GetComponent<MainMenu>().StartGame();
+            //Do w.e you want to do for when the video is done playing.
+
+            //Cancel Invoke since video is no longer playing
+            CancelInvoke("checkOver");
+        }
+    }
+}
